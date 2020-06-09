43aef8fb14e9555b146a366b40fff60aa6d3aa21
Haroon Akram Bajwa <haroonbajwa29@users.noreply.github.com>
2020-06-09 15:18:14 +0300

    box

--- src/Hello.js
+++ src/Hello.js
@@ -3,7 +3,15 @@ import './Hello.css'
 
 function Hello({firstName}) {
   return <p className="myname" >This is para in Hello Tag name : {firstName}</p>
-  
+
+}
+<div> {
+  background-color: lightgrey;
+  width: 300px;
+  border: 15px solid green;
+  padding: 50px;
+  margin: 20px;
+  </div>
 }
 
 export default Hello;
